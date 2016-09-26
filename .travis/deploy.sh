95c8502393ec9b429241694ec1ad3a9b080ef070
Abner Chou <zhcjtht@hotmail.com>
2016-09-26 17:16:03 -0400

    test a change

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,19 +1,9 @@
 
 #!/bin/bash
-# Decrypt the private key
-openssl aes-256-cbc -K $encrypted_5cd716f23a63_key -iv $encrypted_5cd716f23a63_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
-# Set the permission of the key
-chmod 600 ~/.ssh/id_rsa
-# Start SSH agent
-eval $(ssh-agent)
-# Add the private key to the system
-ssh-add ~/.ssh/id_rsa
-# Copy SSH config
-cp .travis/ssh_config ~/.ssh/config
+
 # Set Git config
 git config --global user.name "Abner Chou"
 git config --global user.email contact@abnerchou.me
-# Clone the repository
-git clone --branch gh-pages git@github.com:NoahDragon/en.git .deploy_git
+
 # Deploy to GitHub
 npm run deploy
