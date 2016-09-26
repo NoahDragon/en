e66472f3b175244da43cb872bc5997b7b88ad823
Abner Chou <zhcjtht@hotmail.com>
2016-09-26 17:20:15 -0400

    Remove ssh keys for travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,7 @@
 # Set Git config
 git config --global user.name "Abner Chou"
 git config --global user.email contact@abnerchou.me
-
+# Clone the repository
+git clone --branch gh-pages git@github.com:NoahDragon/en.git .deploy_git
 # Deploy to GitHub
 npm run deploy
