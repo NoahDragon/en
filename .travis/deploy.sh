29ae318ce16f371fcf354baeb040bdea9d6ae8b2
Abner Chou <zhcjtht@hotmail.com>
2016-12-07 21:45:50 -0500

    Purge CDN Caches

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,10 @@
 
 #!/bin/bash
 
+# Purge CDN
+curl "https://api.keycdn.com/zones/purgeurl/$CDN_ZONE_ID.json" -u $CDN_API: -X DELETE -H "Content-Type: application/json" --data '{"urls":["'"$CDN_ZONE_URL"'/index.html","'"$CDN_ZONE_URL"'/tags/index.html", "'"$CDN_ZONE_URL"'/categories/index.html", "'"$CDN_ZONE_URL"'/archives/index.html", "'"$CDN_ZONE_URL"'/sitemap.xml", "'"$CDN_ZONE_URL"'/page-sitemap.xml", "'"$CDN_ZONE_URL"'/post-sitemap.xml", "'"$CDN_ZONE_URL"'/tag-sitemap.xml","'"$CDN_ZONE_URL"'/category-sitemap.xml"]}'
+
+
 # Set Git config
 git config --global user.name "Abner Chou"
 git config --global user.email contact@abnerchou.me
