46fffbf8d05eb191eac62b0ea31706463ff5075c
Abner Chou <zhcjtht@hotmail.com>
2018-07-19 15:44:06 -0400

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,15 @@
-
 #!/bin/bash
+
+# Decrypt the private key
+openssl enc -d -aes256 -in .travis/id_rsa.enc -out ~/.ssh/id_rsa -k $GITHUB_TOKEN
+# Set the permission of the key
+chmod 600 ~/.ssh/id_rsa
+# Start SSH agent
+eval $(ssh-agent)
+# Add the private key to the system
+ssh-add ~/.ssh/id_rsa
+# Copy SSH config
+cp .travis/ssh_config ~/.ssh/config
 
 # Set Git config
 git config --global user.name "Abner Chou"
