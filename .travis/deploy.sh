4b0e4adf93a509294c325bd973f790d9fd1a2d06
Abner Chou <zhcjtht@hotmail.com>
2018-07-19 16:05:59 -0400

    Update _config.yml (#6)
    * Update _config.yml
    
    * Update config.yml
    
    * Update .travis.yml
    
    * Update deploy.sh
    
    * Create CNAME
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Add files via upload
    
    * Create ssh_config
    
    * Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,15 @@
-
 #!/bin/bash
+
+# Decrypt the private key
+openssl enc -d -aes256 -in .travis/id_rsa.enc -out ~/.ssh/id_rsa -k $GITHUB_TOKEN
+# Set the permission of the key
+chmod 600 ~/.ssh/id_rsa
+# Start SSH agent
+eval $(ssh-agent)
+# Add the private key to the system
+ssh-add ~/.ssh/id_rsa
+# Copy SSH config
+cp .travis/ssh_config ~/.ssh/config
 
 # Set Git config
 git config --global user.name "Abner Chou"
@@ -8,5 +18,3 @@ git config --global user.email contact@abnerchou.me
 git clone --branch gh-pages git@github.com:NoahDragon/en.git .deploy_git
 # Deploy to GitHub
 npm run deploy
-# Purge CDN
-curl "https://api.keycdn.com/zones/purgeurl/$CDN_ZONE_ID.json" -u $CDN_API: -X DELETE -H "Content-Type: application/json" --data '{"urls":["'"$CDN_ZONE_URL"'/index.html","'"$CDN_ZONE_URL"'/tags/index.html", "'"$CDN_ZONE_URL"'/categories/index.html", "'"$CDN_ZONE_URL"'/archives/index.html", "'"$CDN_ZONE_URL"'/sitemap.xml", "'"$CDN_ZONE_URL"'/page-sitemap.xml", "'"$CDN_ZONE_URL"'/post-sitemap.xml", "'"$CDN_ZONE_URL"'/tag-sitemap.xml","'"$CDN_ZONE_URL"'/category-sitemap.xml"]}'
